d1181c5262d58decd22b376e40c740270a85bf6c
Paulina <paulina.stojczyk@wp.pl>
2024-10-17 11:29:21 +0200

    adding calculator models FullDayIntake tests

--- ketoApp/calculator/tests/test_models.py
+++ ketoApp/calculator/tests/test_models.py
@@ -20,7 +20,7 @@ class ProductModelTest(TestCase):
         self.user = KetoAppUser.objects.create_user(username='testuser', password='testpassword123')
 
     def test_update_or_create_fulldayintake_creates_new_fulldayintake(self):
-        """Test create products for new fulldayintake"""
+        """Test create products for new FullDayIntake"""
 
         product1 = Product.objects.create(
             date=datetime.date.today(),
@@ -54,7 +54,7 @@ class ProductModelTest(TestCase):
         self.assertEqual(fulldayintake.total_carbs, 1)
 
     def test_update_or_create_fulldayintake_updates_existing_fulldayintake(self):
-        """Test create fulldayintake with existing today's date"""
+        """Test create FullDayIntake with existing today's date"""
 
         FullDayIntake.objects.create(
             date=datetime.date.today(),
@@ -85,7 +85,7 @@ class ProductModelTest(TestCase):
         self.assertEqual(fulldayintake.total_carbs, 28)
 
     def test_update_or_create_fulldayintake_0_values(self):
-        """Test fulldayintake creates with 0 values for nutritional fields"""
+        """Test FullDayIntake creates with 0 values for nutritional fields"""
         Product.objects.all().delete()
 
         product = Product.objects.create(
@@ -130,3 +130,87 @@ class ProductModelTest(TestCase):
         self.assertEqual(fulldayintake.total_fat, 0)
         self.assertEqual(fulldayintake.total_protein, 0)
         self.assertEqual(fulldayintake.total_carbs, 0)
+
+
+class FullDayIntakeModelTest(TestCase):
+
+    def setUp(self):
+        """Creating configuration for all tests"""
+        self.user = KetoAppUser.objects.create_user(username='testuser', password='testpass')
+
+        self.product1 = Product.objects.create(
+            date=datetime.date.today(),
+            name="Egg",
+            grams="100",
+            kcal=155,
+            carb=1,
+            fat=11,
+            protein=13,
+        )
+
+        self.product2 = Product.objects.create(
+            date=datetime.date.today(),
+            name="Chicken Breast",
+            grams="150",
+            kcal=165,
+            carb=0,
+            fat=3,
+            protein=31,
+        )
+
+        self.product1.user.add(self.user)
+        self.product2.user.add(self.user)
+
+    def test_fulldayintake_creation(self):
+        """Test creating FullDayIntake with valid data"""
+        fulldayintake = FullDayIntake.objects.create(
+            date=datetime.date.today(),
+            total_kcal=320,
+            total_carbs=1,
+            total_fat=14,
+            total_protein=44,
+            start=datetime.date.today(),
+        )
+
+        fulldayintake.product.set([self.product1, self.product2])
+        fulldayintake.user.add(self.user)
+
+        self.assertEqual(fulldayintake.total_kcal, 320)
+        self.assertEqual(fulldayintake.total_carbs, 1)
+        self.assertEqual(fulldayintake.total_fat, 14)
+        self.assertEqual(fulldayintake.total_protein, 44)
+        self.assertEqual(fulldayintake.date, datetime.date.today())
+        self.assertIn(self.product1, fulldayintake.product.all())
+        self.assertIn(self.product2, fulldayintake.ptoduct.all())
+        self.assertIn(self.user, fulldayintake.user.all())
+
+    def test_fulldayintake_relation_with_products(self):
+        """Test the ManyToMany relation between FullDayIntake and Product"""
+        fulldayintake = FullDayIntake.objects.create(
+            date=datetime.date.today(),
+            total_kcal=320,
+            total_carbs=1,
+            total_fat=14,
+            total_protein=44,
+        )
+
+        fulldayintake.product.set([self.product1, self.product2])
+
+        self.assertEqual(fulldayintake.product.count(), 2)
+        self.assertIn(self.product1, fulldayintake.product.all())
+        self.assertIn(self.product2, fulldayintake.product.all())
+
+    def test_fulldayintake_relation_with_users(self):
+        """Test the ManyToMany relation between FullDayIntake and KetoAppUser"""
+        fulldayintake = FullDayIntake.objects.create(
+            date=datetime.date.today(),
+            total_kcal=320,
+            total_carbs=1,
+            total_fat=14,
+            total_protein=44,
+        )
+
+        fulldayintake.user.add(self.user)
+
+        self.assertEqual(fulldayintake.user.count(), 1)
+        self.assertIn(self.user, fulldayintake.user.all())
